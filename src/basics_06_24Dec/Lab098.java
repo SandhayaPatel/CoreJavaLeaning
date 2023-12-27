5b60d8fbf3272fcdae178cbc6c3442e85d9036f6
SandhayaPatel <sandhya.sandhyapatel@gmail.com>
2023-12-27 21:24:31 +0530

    My first 17 dec

---
+++ src/basics_06_24Dec/Lab098.java
@@ -0,0 +1,4 @@
+package src.basics_06_24Dec;
+
+public class Lab098 {
+}
---
+++ src/basics_06_24Dec/Lab099.java
@@ -0,0 +1,7 @@
+package src.basics_06_24Dec;
+
+public class Lab099 {
+    public static void main(String[] args) {
+        System.out.println("hekkko");
+    }
+}
